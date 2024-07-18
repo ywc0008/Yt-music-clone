96a5910e1380f04fb8053b9b00ed5cce9d89b02a
wzz <ywc0008@naver.com>
2024-07-18 23:41:49 +0900

    explore category

--- my-app/app/explore/layout.tsx
+++ my-app/app/explore/layout.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div>
-      layout멎너
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default layout;
